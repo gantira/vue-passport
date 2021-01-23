6ce06e14f910e6c54fa696a1d85088505071af9a
Angga Gantira <angga.gantira@gmail.com>
2021-01-23 22:43:37 +0700

    Tutorial Laravel Passport dan Vue Js 3 #5 : Install dan Konfigurasi Axios, Vue Router dan Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//import router
+import router from './router'
+//import Bootstrap, Popper, jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
